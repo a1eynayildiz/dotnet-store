65d8f3c41b684743005cbb73f395c35559810069
Aleyna Yıldız <aleynayildizc@gmail.com>
2026-05-11 18:49:35 +0300

    setup EF Core and add InitialCreate migration

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -4,7 +4,14 @@ namespace dotnet_store.Models;
 /*DataContext sınıfı,Entityframework Core'un DbContext sınıfından türetilir.DbContext ile veritabanı işlemleri yapılır.*/
 public class DataContext:DbContext 
 {
+    //yapıcı metot(constructor)
+    public DataContext(DbContextOptions<DataContext> options):base(options)
+    {
+        
+    }
     public DbSet<Urun> Urunler { get; set; }
-    
-    
 }
+
+
+// //bu sınıfı kullanabilmek için bir nesne üretmeliyiz.
+// DataContext _context = new DataContext();
